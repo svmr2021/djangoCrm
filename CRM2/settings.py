645bb5a1c7ce5b5d216db074d63b87d2d3423f3e
svrm2021 <saidamir2020t@gmail.com>
2021-06-25 15:55:34 +0500

    test commit

--- CRM2/settings.py
+++ CRM2/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = 'static_root'
+# STATIC_ROOT = 'static_root'
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
